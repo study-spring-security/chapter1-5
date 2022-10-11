c1d94903aa02c0879a7c9b3e2c1f26f9eb6bc5dd
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-12 01:51:16 +0900

    feat: Add DummyUser

---
+++ src/main/java/com/example/studyspringsecurity/model/DummyUser.java
@@ -0,0 +1,45 @@
+package com.example.studyspringsecurity.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class DummyUser implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(() -> "READ");
+    }
+
+    @Override
+    public String getPassword() {
+        return "123456";
+    }
+
+    @Override
+    public String getUsername() {
+        return "jung";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
