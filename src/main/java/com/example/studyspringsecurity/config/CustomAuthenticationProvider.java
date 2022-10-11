9c9ef95c234f06cf733bf9c7f7bdd8144ebd19e7
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-12 04:33:21 +0900

    refactor: UserDetailsService 로 변경

--- src/main/java/com/example/studyspringsecurity/config/CustomAuthenticationProvider.java
+++ src/main/java/com/example/studyspringsecurity/config/CustomAuthenticationProvider.java
@@ -1,16 +1,23 @@
 package com.example.studyspringsecurity.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
@@ -18,8 +25,9 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
         String username = authentication.getName();
         String password = String.valueOf(authentication.getCredentials());
 
-        if ("jung".equals(username) && "123456".equals(password)) {
-            return new UsernamePasswordAuthenticationToken(username, password, List.of());
+        UserDetails u = userDetailsService.loadUserByUsername(username);
+        if (passwordEncoder.matches(password, u.getPassword())) {
+            return new UsernamePasswordAuthenticationToken(username, password, u.getAuthorities());
         }
 
         throw new AuthenticationCredentialsNotFoundException("Error in authentication!!");
