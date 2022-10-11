73dc8502ec238e6a05e72cc3fbdcb738a845a183
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-12 02:26:16 +0900

    feat: Add User

--- src/main/java/com/example/studyspringsecurity/model/User.java
+++ src/main/java/com/example/studyspringsecurity/model/User.java
@@ -6,23 +6,37 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-public class DummyUser implements UserDetails {
+public class User implements UserDetails {
+
+    // User 클래스는 변경할 수 없다. 인스턴스를 만들때 세 특성의 값을 지정하며
+    // 이러한 값은 나중에 변경할 수 없다.
+    private final String username;
+    private final String password;
+    // 예제를 간단하게 하기 위해 한 사용자에게 하나의 권한만 적용한다.
+    private final String authority;
+
+    public User(String username, String password, String authority) {
+        this.username = username;
+        this.password = password;
+        this.authority = authority;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(() -> "READ");
+        return List.of(() -> authority);
     }
 
     @Override
     public String getPassword() {
-        return "123456";
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return "jung";
+        return username;
     }
 
+    // 계정은 만료 되거나 잠기지 않는다.
     @Override
     public boolean isAccountNonExpired() {
         return true;
