251db0582bc20a8c17486524db7d68c901c1cac6
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-12 23:16:27 +0900

    refactor: RestController -> Controller 변경

--- src/main/java/com/example/studyspringsecurity/controller/HelloController.java
+++ src/main/java/com/example/studyspringsecurity/controller/HelloController.java
@@ -1,15 +1,20 @@
 package com.example.studyspringsecurity.controller;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello(Authentication a){
         System.out.println("a = " + a);
         return "hello " +a.getName() +"!";
+    }
+
+    @GetMapping("/home")
+    public String home(){
+        return "home.html";
     }
 }
