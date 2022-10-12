ba00781e50f8dee6773030da9111190871b28cde
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-12 23:31:51 +0900

    refactor: add CustomAuthenticationSuccessHandler

---
+++ src/main/java/com/example/studyspringsecurity/config/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,28 @@
+package com.example.studyspringsecurity.config;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        var authorities = authentication.getAuthorities();
+
+        var auth = authorities.stream()
+                .filter(a -> a.getAuthority().equals("read"))
+                .findFirst(); // 'read' 권한이 없으면 비어 있는 Optional 객체 반환
+
+        if (auth.isPresent()) { // 'read'
+            response.sendRedirect("home");
+        } else {
+            response.sendRedirect("/error");
+        }
+    }
+}
