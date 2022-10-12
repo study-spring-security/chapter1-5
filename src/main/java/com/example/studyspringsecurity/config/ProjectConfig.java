8af25d96f6cf985a21aa1fa8442339094bb8eb93
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-12 21:14:15 +0900

    feat: AuthenticationManager 추가

--- src/main/java/com/example/studyspringsecurity/config/ProjectConfig.java
+++ src/main/java/com/example/studyspringsecurity/config/ProjectConfig.java
@@ -1,51 +1,29 @@
 package com.example.studyspringsecurity.config;
 
+import com.example.studyspringsecurity.config.CustomAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.sql.DataSource;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Configuration
 @RequiredArgsConstructor
 public class ProjectConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        String userByUsernameQuery = "select username, password, enabled from users where username = ?";
-        String authsByUserQuery = "select username, authority from authorities where  username = ?";
-
-        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
-
-        userDetailsManager.setUsersByUsernameQuery(userByUsernameQuery);
-        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
-
-        return userDetailsManager;
-    }
+    private final CustomAuthenticationProvider customAuthenticationProvider;
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        Map<String, PasswordEncoder> encoders = new HashMap<>();
-
-        encoders.put("noop", NoOpPasswordEncoder.getInstance());
-        encoders.put("bcrypt", new BCryptPasswordEncoder());
-        encoders.put("scrypt", new SCryptPasswordEncoder());
-
-
-        return new DelegatingPasswordEncoder("bcrypt", encoders);
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        var auth = http.getSharedObject(AuthenticationManagerBuilder.class);
+        auth.authenticationProvider(customAuthenticationProvider);
+        return auth.build();
     }
+
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
