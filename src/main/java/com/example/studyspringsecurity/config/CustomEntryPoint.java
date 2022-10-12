d06a82ebe21f6f5db48b5e19ca755227fe32cdc0
jung-ho-1231 <parkjung-1231@naver.com>
2022-10-12 22:06:41 +0900

    feat: CustomEntryPoint 생성

---
+++ src/main/java/com/example/studyspringsecurity/config/CustomEntryPoint.java
@@ -0,0 +1,19 @@
+package com.example.studyspringsecurity.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomEntryPoint implements AuthenticationEntryPoint {
+    
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        response.addHeader("message", "Luke, I'm your father!");
+        response.sendError(HttpStatus.UNAUTHORIZED.value());
+    }
+}
